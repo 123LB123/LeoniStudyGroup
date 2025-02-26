54c2f8e74c67b696717a0108aaba0d8432c54ce4
Leoni Buss <u23594561@up.ac.za>
2025-02-26 11:09:27 +0200

    Changes

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace u23594561Prac1_test_.Controllers
+{
+	public class PeopleController : Controller
+    {
+        // GET: People
+
+        public ActionResult ListPeople()
+        {
+            List<Models.PersonModel> people = new List<Models.PersonModel>();
+
+            people.Add(new Models.PersonModel { StudentNumber = "u12345678", FirstName = "Scarlett", SurName = "Johansonn", EmailAddress = "scar.jo@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u12345679", FirstName = "John", SurName = "Lennon", EmailAddress = "john.len@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u12345677", FirstName = "Dakota", SurName = "Johnson", EmailAddress = "dak.jo@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u12345678", FirstName = "Lana", SurName = "Del Ray", EmailAddress = "lana.dr@tuks.co.za" });
+            people.Add(new Models.PersonModel { StudentNumber = "u12345678", FirstName = "Gwyneth", SurName = "Paltrow", EmailAddress = "gwyn.pal@tuks.co.za" });
+
+            return View(people);
+
+        }
+
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace u23594561Prac1_test_.Models
+{
+	public class PersonModel
+    {//Quick property - type "prop" press tab x2
+        //Complete the property details
+        //This is the "Model" of "data source"
+
+        [Display(Name = "STUDENT NUMBER")] //Add as decorator
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")] //Add as decorator
+        public string FirstName { get; set; }
+
+        [Display(Name = "SURNAME")] //Add as decorator
+        public string SurName { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")] //Add as decorator
+        // [Required(ErrorMessage = "Name is mandatory")]=> Example of manditory decorator
+        public string EmailAddress { get; set; }
+    }
+}
